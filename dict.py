ebf0932a1db8ad392a3d162c9d6ed37412e63747
antonioGabrielGomes <antonioggca000@gmail.com>
2020-10-26 15:22:40 -0300

    dicionarios

---
+++ dict.py
@@ -0,0 +1,17 @@
+counts = dict()
+names = ['csev', 'cwen', 'csev', 'zqian', 'cwen']
+
+'''
+for name in names: 
+    if name not in counts:
+        counts[name] = 1
+    else:
+        counts[name] = counts[name] + 1
+
+print(counts)
+'''
+
+for name in names: 
+    counts[name] = counts.get(name, 0) + 1
+
+print(counts)
---
+++ exe94.py
@@ -0,0 +1,28 @@
+'''
+9.4 Write a program to read through the mbox-short.txt and figure out who has sent the greatest number of mail messages. The program looks for 'From ' lines and takes the second word of those lines as the person who sent the mail. The program creates a Python dictionary that maps the sender's mail address to a count of the number of times they appear in the file. After the dictionary is produced, the program reads through the dictionary using a maximum loop to find the most prolific committer.
+'''
+
+name = input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+handle = open(name)
+
+result = dict()
+
+for line in handle:
+    line = line.rstrip()
+    if line.startswith('From') and not line.startswith('From:'):
+    	words = line.split()
+        email = words[1]
+    	result[email] = result.get(email, 0) + 1
+        
+
+bigemail = None
+bigvalue = None
+for key,value in result.items():
+    if bigemail is None or value > bigvalue:
+        bigemail = key
+        bigvalue = value
+        
+        
+print(bigemail, bigvalue)
+
